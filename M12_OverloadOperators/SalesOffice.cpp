20b135b4462746ba88905410c3deac9672e6600f
martymcflywa <doc.brown.83@gmail.com>
2018-05-06 19:58:46 +0800

    Module 12 workshop complete.

---
+++ M12_OverloadOperators/SalesOffice.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "SalesOffice.h"
+
+SalesOffice::SalesOffice(std::string officeName, double sales) : _officeName(officeName), _sales(sales)
+{
+}
+
+std::string SalesOffice::name() const
+{
+    return _officeName;
+}
+
+double SalesOffice::operator/(const SalesOffice salesOffice) const
+{
+    return _sales / salesOffice._sales * 100;
+}
+
+std::ostream& operator<<(std::ostream& out, const SalesOffice& salesOffice)
+{
+    out << "The " + salesOffice.name() << " office sold $" + std::to_string(salesOffice._sales) + '\n';
+    return out;
+}
---
+++ M12_OverloadOperators/SalesOffice.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <string>
+
+class SalesOffice
+{
+    friend std::ostream& operator<<(std::ostream& out, const SalesOffice& salesOffice);
+
+    std::string _officeName;
+    double _sales;
+public:
+    SalesOffice(std::string officeName, double sales);
+    std::string name() const;
+    double operator/(SalesOffice salesOffice) const;
+};
