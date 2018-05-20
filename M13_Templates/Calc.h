7ad0b2b9a47eb81a9e7a75e3880db5a9b61c7732
Martin Ponce <doc.brown.83@gmail.com>
2018-05-20 16:21:30 +0800

    Module 13 workshop complete.

---
+++ M13_Templates/Calc.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+class Calc
+{
+public:
+    template<typename T>
+    static T multiply(T a, T b);
+
+    template<typename T>
+    static T add(T a, T b);
+};
+
+template <typename T>
+T Calc::multiply(T a, T b)
+{
+    return a * b;
+}
+
+template <typename T>
+T Calc::add(T a, T b)
+{
+    return a + b;
+}
